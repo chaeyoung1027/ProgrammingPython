9437e495ceccae8d008170168af800e5e7b79471
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-03-22 10:00:30 +0900

    helloworld, print()

---
+++ 20220322_helloworld.py
@@ -0,0 +1,6 @@
+# ""와 ''의 구분이 (없음)
+print("Hello, World!")
+print('Hello, 임채영')
+print('Red velvet\'s favorite song is Red flavour')
+print("Red velvet's favorite song is Red flavour")
+#문자 ' = \'로 표현할 수 있음.
