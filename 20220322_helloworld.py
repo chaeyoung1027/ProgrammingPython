469cde022bd3cd42594b20fac6fbd195732a9c18
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-03-22 10:14:13 +0900

    변수 선언, 초기화, 여러가지 출력하는 방법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,6 +1,11 @@
 # ""와 ''의 구분이 (없음)
 print("Hello, World!")
-print('Hello, 임채영')
+name = "양다연"
+print('Hello, '+name+'!') #java 형식
+print('Hello, &s' % name) #c 형식
+print('Hello, {}'.format(name)) #format함수
+print(f'hello,{name}!') # f-string 형식
+
 print('Red velvet\'s favorite song is Red flavour')
 print("Red velvet's favorite song is Red flavour")
 #문자 ' = \'로 표현할 수 있음.
