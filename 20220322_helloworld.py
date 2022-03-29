4ab409494c0ca0be3c61622dcf89c50c94e6a794
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-03-29 10:18:13 +0900

    변수, int, float, str, \n, type(), // : 몫, % : 나머지

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,11 +1,16 @@
 # ""와 ''의 구분이 (없음)
-print("Hello, World!")
+print("Hello, World!") #""와 ''가 같다.
 name = "양다연"
 print('Hello, '+name+'!') #java 형식
-print('Hello, &s' % name) #c 형식
+print('Hello, &s' + name) #c 형식
 print('Hello, {}'.format(name)) #format함수
-print(f'hello,{name}!') # f-string 형식
+print(f'hello,{name}!') # f-string 형식#f-string형식
 
 print('Red velvet\'s favorite song is Red flavour')
 print("Red velvet's favorite song is Red flavour")
 #문자 ' = \'로 표현할 수 있음.
+
+student_number = "2113"
+name = "임채영"# ",'로 묶지 않으면 변수로 봄.
+print(f"학번 : "+student_number+", 이름 : "+name)# 앞에 f : format
+print(f"학번 : {student_number}, 이름 : {name}")
---
+++ 20220329_type.py
@@ -0,0 +1,28 @@
+#숫자
+student_number = 2113
+age = 18
+height = 165.7
+
+#문자
+name = '임채영'
+
+print(f'학번 : {student_number}\n이름 : {name}\n나이 : {age}\n키 : {height}\n')  #학번 : 2213
+
+print(type(student_number)) #<class 'int'>
+print(type(name)) #<class 'str'>
+print(type(age))#<class 'int'>
+print(type(height))#<class 'float'>
+print(type(10.27))#<class 'float'>
+print(type(1027))#<class 'int'>
+print(type('1027'))#<class 'str'>
+print((10/27))#java : 0, python/ 0.37037037037037035
+print((27/10))#java : 2, python/ 2.7
+#java : 몫 = /, 나머지 = %  python : 우리가 알고있는 나누기(float형이 됨)
+print(27//10) #python : 나눈 몫을 구할 때 / 2개
+print(27%10) #python : 나머지 : 7
+print(type(10/27))#<class 'float'>
+print(type(10//27))#<class 'int'>
+print(10/5) #2.0 <class 'float'>
+
+
+
