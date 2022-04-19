1c9c4af5662b1fbe0178c7b65e19528e4f95f359
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-04-19 10:10:20 +0900

    str 연습. 문자열 함수(len, count, upper, lower, strip, replace, find, index), in, split, join

--- 20220419_str.py
+++ 20220419_str.py
@@ -37,3 +37,39 @@ print(f'{student_number[0:2]}''학년반')
 print(f'{student_number[0:-2]}''학년반')
 print(f'{student_number[:-2]}''학년반') #start : end-1 [: end-1] : 앞까지
 print(f'f{student_number[:]}')         #start : end-1 [ : ] : 앞~ 끝까지
+
+#문자열 함수
+print(f'길이 : {len(student_number)}') #4
+print(f'2의 개수 : {student_number.count("2")}')
+print(f'{"NCT dream darling".upper()}') #다 대문자
+print(f'{"NCT dream darling".lower()}') #다 소문자
+s = "       NCT dream buffering         "
+print(f'{s.strip()}')   #공백 벗기기
+print(f'{s.rstrip()}')  #오른쪽 벗기기
+print(f'{s.lstrip()}')  #왼쪽 벗기기
+print(f'{s.find("d")}') #[7]
+print(f'{s.rfind("d")}') #오른쪽부터 찾기
+print(f'{s.find("z")}') #없으면 -1
+print(f'{s.count("e")}')   #e의 개수세기
+#print(f'{s.index("d")}') #없으면 에러(ValueError : substring not found)
+print(f'{s.replace("buffering", "hello future")}')     #바꾸기(원본이 바뀌는 것이 아니라 바뀐 문자열로 return만 함.
+print(s)
+
+#있나 없나(true or false)
+print('e' in s)
+print('z' in s)
+
+#split, join
+#split : 쪼개기
+ip = '10.253.123.419'
+ip_list = ip.split('.')
+print(ip_list)
+names = '양다연, 최자윤, 임채영, 이예진, 인소리'
+names_list = names.split(',')
+print(names_list)
+print(names_list[2 : 4])
+ip_list_str = '::'.join(ip_list)
+print(ip_list_str)
+names_list_str = ' | '.join(names_list)
+print(names_list_str)
+print(", ".join(names_list))
