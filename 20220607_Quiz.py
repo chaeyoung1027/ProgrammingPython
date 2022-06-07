442510e571789a90f45d0126fcd4875a622049a5
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-06-07 09:55:45 +0900

    Quiz. 학번 -> 학년, 반, 학과, 번호/ if문을 사용하지 않고

--- 20220607_Quiz.py
+++ 20220607_Quiz.py
@@ -16,3 +16,21 @@ phone_number = phone_number.replace(' ','')
 print(phone_number)
 
 print('-'*40)
+
+#2. 학번 -> 학년, 반, 학과, 번호 출력하기
+student_number = '2108'
+# #2학년 1반 뉴미디어소프트웨어과 08번
+# if student_number[1] == '1' or student_number[1]=='2' :
+#     g = '뉴미디어소프트웨어과'
+# elif student_number[1] == '3' or student_number[1]=='4':
+#     g = '뉴미디어웹솔루션과'
+# elif student_number[1] == '5' or student_number[1]=='6':
+#     g = '뉴미디어디자인과'
+
+majors = ['뉴미디어소프웨어과', '뉴미디어소프웨어과', '뉴미디어웹솔류션과', '뉴미디어웹솔류션과', '뉴미디어디자인과', '뉴미디어디자인과']
+index = int(student_number[1])
+g = majors[index-1]
+
+print(f'{student_number[0]}학년 {student_number[1]}반 {g} {int(student_number[2:])}번 ')
+
+print('-'*40)
