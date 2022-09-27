75739c71e536a1810ad996778cbd0abb659fe99c
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-09-27 09:49:29 +0900

    수정 commit **변수, 함수, 클래스명 명확하게**

--- 20220920_Test.py
+++ 20220920_Test.py
@@ -7,8 +7,17 @@
 사람 - 이름, 나이, 성별
 선생님 - 과목, 담임반
 '''
+'''
+클래스는 웬만하면 단수로, 단 여러개 담으면 복수
+수치가 들어오는 값이면, 가능하면 숫자로 세팅
+나이, 생년 등은 숫자, 핸드폰번호는 문자로 처리하자
+01012345678릉 10억 1234만 5678이 아님 => '01012345678' - 인덱싱, 슬라이싱이 편함
 
-class People:
+변수, 함수, 클래스명 명확하게
+
+이제 더이상 내 코드는 나만 보지 않는다. 누구나 내 코드를 읽을 수 있도록 짜자
+'''
+class Person:
     def __init__(self, name):
         self.name = name
         self.age = None
@@ -25,23 +34,21 @@ class People:
         return f'{self.name}\t{self.age}\t{self.gender}'
 
 # people에 값넣기
-고나근샘 = People('고낙은')
-고나근샘.set_age('34')
+고나근샘 = Person('고낙은')
+고나근샘.set_age(34)
 고나근샘.set_gender('남자')
 print(고나근샘)
 
-class Teacher(People):
-    def subject(self, sub):
-        self.sub = sub
-    def HRT(self, hrt):
-        self.hrt = hrt
+class Teacher(Person):
+    def set_subject(self, subject):
+        self.subject = subject
+    def set_homeroom_teacher(self, homeroom_teacher):
+        self.homeroom_teacher = homeroom_teacher
     def __str__(self):
-        return f'{super().__str__()}\t담당과목: {self.sub}\t담임반: {self.hrt}'
-
+        return f'{super().__str__()}\t담당과목: {self.subject}\t담임반: {self.homeroom_teacher}'
 
 #Teacher에 값넣기
 고낙은샘 = Teacher('고낙은')
-
-고낙은샘.subject('체육')
-고낙은샘.HRT('2-2')
+고낙은샘.set_subject('체육')
+고낙은샘.set_homeroom_teacher('2-2')
 print(고낙은샘)
