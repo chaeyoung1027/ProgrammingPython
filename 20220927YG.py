b82f3db0c1ce4e42a9114ddb6cb04f54a208d14a
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-09-27 09:20:44 +0900

    같은 수업을 듣는 친구들의 코드 주제 : 음식 음식 - 길거리 음식 수정 commit

--- 20220927YG.py
+++ 20220927YG.py
@@ -1,50 +1,57 @@
-class food:
+class Food:
     def __init__(self, name):
         self.name = name
         self.style = None
         self.money = 0
-    def setStyle(self, style):
+
+    def set_style(self, style):
         self.style = style
-    def setMoney(self, money):
+
+    def set_money(self, money):
         self.money = money
+
     def __str__(self):
-        return f'{self.name}\t 비용 : {self.money}\t style : {self.style}\t'
+        return f'{self.name}\t 비용 : {self.money}원\t style : {self.style}\t'
 
 
-
-class delivery(food):
+class DeliveryFood(Food):
     def __init__(self, name):
         super().__init__(name)
-        self.de_m = 0
-    def setDe_m(self, de_m):
-        self.de_m = de_m
+        self.delivery_money = 0
+
+    def set_delivery_money(self, de_m):
+        self.delivery_money = de_m
+
     def __str__(self):
-        return f'{super().__str__()}\t 배달료 : {self.de_m}'
+        return f'{super().__str__()}\t 배달료 : {self.delivery_money}'
 
-치킨 = delivery('처갓집')
-치킨.setStyle('튀김')
-치킨.setMoney(36000)
-치킨.setDe_m(2500)
-print(치킨.__str__())
 
-class street(food):
+치킨 = DeliveryFood('처갓집')
+치킨.set_style('튀김')
+치킨.set_money(36000)
+치킨.set_delivery_money(2500)
+print(치킨)
+
+
+class StreetFood(Food):
     def __init__(self, name):
         super().__init__(name)
         self.shape = None
-        self.Season = None
+        self.season = None
 
-    def setShape(self, shape):
+    def set_shape(self, shape):
         self.shape = shape
 
-    def setSeason(self,Season):
-        self.Season = Season
+    def set_season(self, season):
+        self.season = season
 
     def __str__(self):
-        return f'{super().__str__()}\t 매장 형태 : {self.shape}\t 계절 : {self.Season}'
+        return f'{super().__str__()}\t 매장 형태 : {self.shape}\t 계절 : {self.season}'
 
-붕어빵 = street('황금 붕어빵')
-붕어빵.setStyle('빵')
-붕어빵.setMoney(5000)
-붕어빵.setShape('수레')
-붕어빵.setSeason('겨울')
-print(붕어빵.__str__())
+
+붕어빵 = StreetFood('황금 붕어빵')
+붕어빵.set_Style('빵')
+붕어빵.set_money(5000)
+붕어빵.set_shape('수레')
+붕어빵.set_season('겨울')
+print(붕어빵)
