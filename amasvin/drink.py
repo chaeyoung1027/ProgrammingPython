3ad01c98afa30c6b1ae036b293bd1ffbc7162d53
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-10-25 10:10:52 +0900

    Drink 값 클래스 변수 [인덱스]로 저장, set_cup_size()

--- amasvin/drink.py
+++ amasvin/drink.py
@@ -15,18 +15,31 @@
 
 class Drink:
     #클래스 변수
-    _CUP_SIZE = ('레귤러','점보')    #0 : 레귤러, 1 : 점보
+    _CUP_SIZES = ('레귤러','점보')    #0 : 레귤러, 1 : 점보
     _SUGARS = ('30%','50%','70%','100%')    #0:30%, 1:50%, 3:70%, 4:100%
     _ICES = ('없음','적게','기본','많게')
     def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.cup_size = '레귤러'
-        self.cup.suger = '50%'
-        self.ice = '기본'
+        self.cup_size = 0
+        self.sugar = 1  #0:30%, 1:50%, 3:70%, 4:100%
+        self.ice = 2
     def __str__(self):
-        return f'이름 : {self.name}\t가격 : {self.price}원\t컵사이즈: {self.cup_size}\t당도: {self.sugar}\t얼음량: {self.ice}'  #self 꼭 쓰기
+        return f'이름 : {self.name}\t가격 : {self.price}원\t컵사이즈: {Drink._CUP_SIZES[self.cup_size]}\t당도: {Drink._SUGARS[self.sugar]}\t얼음량: {Drink._ICES[self.ice]}'  #self 꼭 쓰기
+    def set_cup_size(self):
+        #사용자에게 숫자를 묻자 1 : 레귤러, 2:점보
+        for index, cup_size_label in enumerate(Drink._CUP_SIZES):   #*enumerate*****
+            print(f'{index+1}.{cup_size_label}')
+        cup_size = input('컵사이즈를 선택하세요 : ')
+        if cup_size == '' : #그냥 엔터치면 기본값 넣자
+            self.cup_size = 0
+        else:
+            self.cup_size = int(cup_size)-1
+        #self.cup_size가 점보(2)일 때, 가격 +500원
+        if self.cup_size == 1:
+            self.price+=500
 
 음료1 = Drink("아메리카노", 1800)
+음료1.set_cup_size()
 print(음료1)
 
