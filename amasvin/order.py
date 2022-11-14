906dbbeef0a8828f7bd30e02cd88c8f4762a7454
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-11-15 08:49:33 +0900

    order

---
+++ amasvin/order.py
@@ -0,0 +1,22 @@
+class Order:
+    def __init__(self):
+        self.menu = []  # 메뉴판
+        self.init_menu()
+        self.order_menu = []    # 주문한 음료수 리스트
+
+    def __str__(self):
+        pass
+
+    def init_menu(self):    # 메뉴판 초기화
+        pass
+
+    def order(self):
+        # 반복
+            self.show_menu()    # 메뉴판 보여주기
+            # 음료수 고르지(사용자 입력)
+            # 새로운 음료수로 생성하고, 옵션들 주문
+            # 주문한 음료수 리스트에 새로운 음료수 추가하기
+        # 주문한 음료수 리스트 출력
+
+    def show_menu(self):
+        pass
