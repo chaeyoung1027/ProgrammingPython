0108e738c70f1eca7b9ffc12fec9534a08e49720
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-11-29 09:35:57 +0900

    import 모듈; 모듈.함수() or 모듈.변수()

---
+++ module, package/gugudan.py
@@ -0,0 +1,6 @@
+def gugudan(dan):
+    for i in range(1, 10):
+        print(f'{dan} * {i} = {dan * i}')
+
+if __name__ == '__main__':
+    gugudan(3)
--- module, package/insa2.py
+++ module, package/insa2.py
@@ -1,4 +1,6 @@
 import Hello2
 import hello_func as h   #import 뒤에 있는 hello_func는 무조건 사용해야한다.
+import gugudan as g
 
 h.hello()
+g.gugudan(5)    #gugudan.py의 gugudan()함수를 사용한다.
