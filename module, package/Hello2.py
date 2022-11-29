9b0871de79877626e2a52c995dfd649880be671b
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-11-29 09:15:35 +0900

    module 연습:import, if __name__ == '__main__':

---
+++ module, package/Hello2.py
@@ -0,0 +1,7 @@
+#file과 module은 같다.
+#module, package/Hello2.py
+#modlue들의 모음 : package
+#mudule의 이름과 file의 이름은 같다.
+
+print('hello2 모듈입니다.')
+print('hello world')
---
+++ module, package/insa2.py
@@ -0,0 +1,4 @@
+import Hello2
+import hello_func   #import 뒤에 있는 hello_func는 무조건 사용해야한다.
+
+hello_func.hello()
